91f9a411e9a2cd98ae4074b12193b2eb6961efb4
MaciejBartos <maciejbartos@outlook.com>
2019-11-04 15:10:57 +0100

    zmiana sposobu poruszania sie wilka oraz drobny refactoring

--- zad2/sheepWolfGame.py
+++ zad2/sheepWolfGame.py
@@ -24,23 +24,23 @@ class Wolf:
 
 class WolfSheepGame:
     __number_of_turns = None
-    __number_of_sheeps = None
+    __number_of_sheep = None
     __init_pos_limit = None
     __sheep_move_dist = None
     __wolf_move_dist = None
-    __sheeps_position = list()
+    __sheep_position = list()
     __wolf_position = list()
 
     def __init__(self, number_of_turns, number_of_sheeps, init_pos_limit, sheep_move_dist, wolf_move_dist):
         self.__number_of_turns = number_of_turns
-        self.__number_of_sheeps = number_of_sheeps
+        self.__number_of_sheep = number_of_sheeps
         self.__init_pos_limit = init_pos_limit
         self.__sheep_move_dist = sheep_move_dist
         self.__wolf_move_dist = wolf_move_dist
         self.__wolf_position = Wolf(0)
 
-        for sheep_numer in range(1, number_of_sheeps + 1):
-            self.__sheeps_position.append(Sheep(self.__init_pos_limit, sheep_numer))
+        for sheep_number in range(1, number_of_sheeps + 1):
+            self.__sheep_position.append(Sheep(self.__init_pos_limit, sheep_number))
 
     def count_wolf_to_sheep_distance(self, sheep_position_x, sheep_position_y):
         x_square_distance = (sheep_position_x - self.__wolf_position.x) ** 2
@@ -49,55 +49,64 @@ class WolfSheepGame:
         return distance
 
     def select_closest_sheep(self):
-        closest_sheep = self.__sheeps_position[0]
+        closest_sheep = self.__sheep_position[0]
         closest_sheep_distance = self.count_wolf_to_sheep_distance(closest_sheep.x, closest_sheep.y)
-        for sheep in self.__sheeps_position:
+        for sheep in self.__sheep_position:
             distance = self.count_wolf_to_sheep_distance(sheep.x, sheep.y)
             if distance < closest_sheep_distance:
                 closest_sheep = sheep
         return closest_sheep
 
     def move_sheeps(self):
-        for sheep in self.__sheeps_position:
-            random_position = random.randint(0, 3)
-            # move to east
-            if random_position == 0:
-                sheep.x += self.__sheep_move_dist
+        for sheep in self.__sheep_position:
+            x_direction = random.randint(0, 1)
+            minus_or_plus = random.randint(0, 1)
 
-            # move to west
-            elif random_position == 1:
-                sheep.x -= self.__sheep_move_dist
-
-            # move to north
-            elif random_position == 2:
-                sheep.y += self.__sheep_move_dist
-
-            # move to south
-            elif random_position == 3:
-                sheep.y -= self.__sheep_move_dist
+            if x_direction:
+                sheep.x += (-1) * minus_or_plus + self.__sheep_move_dist
+            else:
+                sheep.y += (-1) * minus_or_plus + self.__sheep_move_dist
 
     def check_if_wolf_in_range(self, sheep):
         if self.__wolf_move_dist >= self.count_wolf_to_sheep_distance(sheep.x, sheep.y):
-            self.__sheeps_position.remove(sheep)
-            print()
-            print("Owca nr: " + str(sheep.id) + " zostala zjedzona!")
-            print()
             return True
         else:
             return False
 
-    def select_wolf_direction(self, sheep):
+    def eat_sheep(self, sheep):
+        self.__sheep_position.remove(sheep)
+        self.print_eaten_sheep(sheep)
+
+    def move_wolf(self, sheep):
         x = sheep.x
         y = sheep.y
         distance = self.count_wolf_to_sheep_distance(x, y)
-        if distance > self.count_wolf_to_sheep_distance(x + self.__wolf_move_dist, y):
-            self.__wolf_position.x -= self.__wolf_move_dist
-        elif distance > self.count_wolf_to_sheep_distance(x - self.__wolf_move_dist, y):
-            self.__wolf_position.x += self.__wolf_move_dist
-        elif distance > self.count_wolf_to_sheep_distance(x, y + self.__wolf_move_dist):
-            self.__wolf_position.y -= self.__wolf_move_dist
-        elif distance > self.count_wolf_to_sheep_distance(x, y - self.__wolf_move_dist):
-            self.__wolf_position.y += self.__wolf_move_dist
+        union_vector = ((x - self.__wolf_position.x) / distance, (y - self.__wolf_position.y) / distance)
+        self.__wolf_position.x += union_vector[0] * self.__wolf_move_dist
+        self.__wolf_position.y += union_vector[1] * self.__wolf_move_dist
+
+    def wolf_turn(self, sheep):
+        if not self.check_if_wolf_in_range(sheep):
+            self.move_wolf(sheep)
+        else:
+            self.eat_sheep(sheep)
+
+    def make_turn(self):
+        self.move_sheeps()
+        self.wolf_turn(self.select_closest_sheep())
+
+    def start_game(self):
+        for turn in range(1, self.__number_of_turns + 1):
+            self.print_current_status(turn)
+            self.make_turn()
+            if len(self.__sheep_position) == 0:
+                break
+        self.print_end_game_status()
+
+    def print_eaten_sheep(self, sheep):
+        print()
+        print("Owca nr: " + str(sheep.id) + " zostala zjedzona!")
+        print()
 
     def print_current_status(self, turn):
         print("Tura " + str(turn))
@@ -106,23 +115,11 @@ class WolfSheepGame:
         print()
 
     def print_end_game_status(self):
-        if len(self.__sheeps_position) == 0:
+        if len(self.__sheep_position) == 0:
             print("Koniec gry, Owce zjedzone!")
         else:
             print("Koniec gry, Owce przezyly!")
 
-    def move_wolf(self, sheep):
-        if not self.check_if_wolf_in_range(sheep):
-            self.select_wolf_direction(sheep)
 
-    def make_turn(self):
-        for turn in range(1, self.__number_of_turns + 1):
-            self.print_current_status(turn)
-            self.move_sheeps()
-            self.move_wolf(self.select_closest_sheep())
-            if len(self.__sheeps_position) == 0:
-                break
-        self.print_end_game_status()
+WolfSheepGame(500, 10, 10.0, 0.5, 1.0).start_game()
 
-
-WolfSheepGame(500, 10, 10.0, 0.5, 0.25).make_turn()
